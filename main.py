df9292b1199c5c6d28796af23e92ca0f453b29ed
unknown <javierluengogutierrez@gmail.com>
2023-02-08 21:29:13 +0000

    First commit from fraluegut2

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+def suma(a: int, b: int):
+    print(a+b)
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +13,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    suma(1,2)
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
